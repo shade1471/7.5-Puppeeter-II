8fbafdd45308e2485eda6cdc5dc8d3ed6e7ed35b
Andrey Gribanov <shade1471@gmail.com>
2022-05-31 15:15:38 +0700

    Edit task 1

--- appCinema.test.js
+++ appCinema.test.js
@@ -1,18 +1,14 @@
-const { expect } = require("chai");
+const { clickElement, putText, getText } = require("./lib/commands.js");
 const {
-  clickElement,
-  putText,
-  getText,
-  orderPlace,
-  checkStatus,
-} = require("./lib/commands.js");
-const { generateName } = require("./lib/util.js");
+  orderTickets,
+  checkSuccess,
+  checkSeatIsTaken,
+} = require("./lib/util.js");
 
 let page;
-
-function needPlace(row, seat) {
-  return `.buying-scheme__wrapper :nth-child(${row}) :nth-child(${seat})`;
-}
+let tomorrow = "nav.page-nav > a:nth-child(2)"; // Билеты на завтра
+let oneWeek = "nav.page-nav > a:nth-child(7)"; // Билеты через неделю
+let movieTime = "[data-seance-id='93']"; // 19:00, Hercules, Movie1
 
 beforeEach(async () => {
   page = await browser.newPage();
@@ -23,44 +19,39 @@ afterEach(() => {
   page.close();
 });
 
-describe("Netology.ru tests", () => {
+describe("Service for Movie tickets order", () => {
   beforeEach(async () => {
     page = await browser.newPage();
     await page.goto("http://qamid.tmweb.ru/client/index.php");
   });
 
-  test("Should order ticket for Movie-1 at 19:00", async () => {
-    await clickElement(page, '[data-seance-id="93"]');
-    await clickElement(page, needPlace(1, 5));
-    await clickElement(page, "button.acceptin-button"); //Нажать забронировать
-    let timeMovie = await getText(page, ".ticket__start");
-    expect(timeMovie).equal("19:00");
-    await clickElement(page, ".acceptin-button");
+  test("Should order one ticket for Movie-1 tomorrow", async () => {
+    await orderTickets(page, tomorrow, movieTime, 3, 1);
+    await checkSuccess(
+      page,
+      "Покажите QR-код нашему контроллеру для подтверждения бронирования."
+    );
   });
 
-  test.only("The second test should to check that seat is not free", async () => {
-    await clickElement(page, '[data-seance-id="93"]');
-    await clickElement(page, needPlace(1, 5));
-    await clickElement(page, "button.acceptin-button");
-    await page.waitForSelector(".ticket__check-title");
+  test("Should order three tickets for Movie-1 in a week", async () => {
+    await orderTickets(page, oneWeek, movieTime, 6, 1, 2, 3);
+    await checkSuccess(
+      page,
+      "Покажите QR-код нашему контроллеру для подтверждения бронирования."
+    );
   });
 
-  test("The first link leads on 'Медиа' page", async () => {
-    await clickElement(page, "header a + a");
-    const actual = await getText(page, ".logo__media");
-    await expect(actual).toContain("Медиа");
+  test("Check if the place is taken after ordering ", async () => {
+    await orderTickets(page, oneWeek, movieTime, 1, 1);
+    await checkSuccess(
+      page,
+      "Покажите QR-код нашему контроллеру для подтверждения бронирования."
+    );
+    await page.goto("http://qamid.tmweb.ru/client/index.php");
+    await checkSeatIsTaken(page, oneWeek, movieTime, 1, 1);
   });
-});
 
-test("Should look for a course", async () => {
-  await page.goto("https://netology.ru/navigation");
-  await putText(page, "input", "тестировщик");
-  const actual = await page.$eval("a[data-name]", (link) => link.textContent);
-  const expected = "Тестировщик ПО";
-  expect(actual).toContain(expected);
-});
-
-test("Should show warning if login is not email", async () => {
-  await page.goto("https://netology.ru/?modal=sign_in");
-  await putText(page, 'input[type="email"]', generateName(5));
+  test("Check if the place is taken", async () => {
+    await checkSeatIsTaken(page, tomorrow, movieTime, 10, 10);
+  });
 });
